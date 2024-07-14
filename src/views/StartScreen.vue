5bb14ec3859d498133c459e43b49948dc0e0c267
monwabisi-gaga <157282815+monwabisi-BG@users.noreply.github.com>
2024-07-14 10:46:47 +0200

    :sparkles: start game view

---
+++ src/views/StartScreen.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex items-center justify-center h-screen bg-gray-100">
+    <div class="text-center">
+      <h1 class="text-4xl font-bold mb-8">Who Wants to Be a Millionaire?</h1>
+      <button @click="startGame" class="bg-green-500 text-white px-4 py-2 rounded-lg">Start Game</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  methods: {
+    ...mapActions(['startGame']),
+    startGame() {
+      this.startGame().then(() => {
+        this.$router.push('/game');
+      });
+    },
+  },
+};
+</script>
